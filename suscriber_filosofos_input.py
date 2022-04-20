2f2914079f0a32582daa010ed237d03eb7c6a971
EstebanJJP <98412172+EstebanJJP@users.noreply.github.com>
2022-04-20 10:33:01 +0200

    añadiendo broker wild

--- suscriber_filosofos_input.py
+++ suscriber_filosofos_input.py
@@ -6,6 +6,7 @@ def on_message(client, userdata, message):
     print(str(message.payload.decode("utf-8")))
 
 mqttBroker = "mqtt.eclipseprojects.io"
+# mqttBroker = "wild.mat.ucm.es"
 client = mqtt.Client()
 client.connect(mqttBroker)
 
@@ -15,4 +16,4 @@ client.subscribe(topic)
 client.on_message = on_message
 time.sleep(30)
 client.loop_stop()
-print("Gracias por echar un vistazo a la mesa de los "+str(topic)+". ¡Vuelve cuando quieras!")
+print("Gracias por echar un vistazo a "+str(topic)+". ¡Vuelve cuando quieras!")

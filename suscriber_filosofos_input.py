fd19be61394c515533671ee8ef9d61fc6f02c0e5
EstebanJJP <98412172+EstebanJJP@users.noreply.github.com>
2022-04-20 11:31:00 +0200

    Update suscriber_filosofos_input.py

--- suscriber_filosofos_input.py
+++ suscriber_filosofos_input.py
@@ -12,7 +12,7 @@ client = mqtt.Client()
 client.connect(mqttBroker)
 
 client.loop_start()
-topic = input("Mesas disponibles:\nMesa_antigua\nMesa_edad_moderna\nMesa_contemporanea\nMesa_actualidad\n¿A qué mesa desea suscribirse? ")
+topic = input("Mesas disponibles:\nMesa_edad_moderna\nMesa_contemporanea\nMesa_actualidad\n¿A qué mesa desea suscribirse? ")
 client.subscribe(topic)
 client.on_message = on_message
 time.sleep(30)
